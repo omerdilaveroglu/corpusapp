a3f6efeb290ce692305cfd49cc41af1520ab281b
Ömer Dilaveroğlu <77552731+omerdilaveroglu@users.noreply.github.com>
2024-07-28 16:10:01 +0300

    intro4

--- dashboard/urls.py
+++ dashboard/urls.py
@@ -4,4 +4,5 @@ from dashboard import views
 
 urlpatterns = [
     path('',views.index,name='home_page'),
+    path('hour_production',views.hour_production,name='hour_production'),
 ]
--- dashboard/views.py
+++ dashboard/views.py
@@ -4,4 +4,7 @@ from django.shortcuts import render,redirect
 # Create your views here.
 
 def index(request):
-    return render(request=request,template_name='account/index.html')
+    return render(request=request,template_name='index.html')
+
+def hour_production(request):
+    return render(request=request,template_name='hour_production.html')
