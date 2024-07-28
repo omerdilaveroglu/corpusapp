69ee4aca26e707c4df4a45de9f047bc77d85310a
Ömer Dilaveroğlu <77552731+omerdilaveroglu@users.noreply.github.com>
2024-07-28 10:54:37 +0300

    intro

--- static/js/sidebar.js
+++
@@ -1,6 +0,0 @@
-let btn = document.querySelector(".toggle-btn");
-let sideBar = document.querySelector(".sidebar");
-
-btn.addEventListener("click", function () {
-  sideBar.classList.toggle("expand");
-});
